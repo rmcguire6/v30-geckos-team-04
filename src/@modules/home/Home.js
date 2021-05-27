505f372086b66624b5ab17b05bebcb5ce4ea139a
Regina Mc Guire <28935544+rmcguire6@users.noreply.github.com>
2021-05-27 14:04:31 -0700

    feat: Add SearchBar to Home component

--- src/@modules/home/Home.js
+++ src/@modules/home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react-dom';
+import SearchBar from '../search/SearchBar'
 
 const Home = () => {
   return (
     <div>
       <h1>Clean the Air</h1>
       <h2>Home</h2>
+      <SearchBar />
     </div>
   );
 };
