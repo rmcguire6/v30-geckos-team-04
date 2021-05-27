d5492532910caa917b11afcc26972a267f657225
Regina Mc Guire <28935544+rmcguire6@users.noreply.github.com>
2021-05-27 14:07:07 -0700

    task: Remove unused AppRouter.module.css

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import styles from './AppRouter.module.css';
 import Mission from '../@modules/mission/Mission';
 import About from '../@modules/about/About';
 import Home from '../@modules/home/Home';
