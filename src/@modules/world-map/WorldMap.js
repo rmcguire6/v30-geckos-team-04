2d4b5e61458474a65fcbd3e5fcf28aa4313f6454
Soyoung <cookr341@gmail.com>
2021-06-03 00:41:04 +0900

    refactor: Remove a warning

--- src/@modules/world-map/WorldMap.js
+++ src/@modules/world-map/WorldMap.js
@@ -28,7 +28,7 @@ const WorldMap = ({ setTooltipContent }) => {
       height={100}
       style={{ width: '100%', height: '70vh' }}
       projectionConfig={{
-        scale: window.innerWidth > 1024 ? 40 : 120,
+        scale: windowWidth > 1024 ? 40 : 120,
       }}
     >
       <ZoomableGroup center={[0, 6]}>
