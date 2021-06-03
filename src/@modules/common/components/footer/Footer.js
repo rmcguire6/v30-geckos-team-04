3932111b579d929854f660be77978993c40348c5
Soyoung <cookr341@gmail.com>
2021-06-03 19:36:17 +0900

    task: Add footer component

--- src/@modules/common/components/footer/Footer.js
+++ src/@modules/common/components/footer/Footer.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import styles from './Footer.module.css';
 
 const Footer = props => {
-  return <h1>Footer</h1>;
+  return (
+    <footer className={styles.footer}>
+      &copy; 2021 Chingu V30 Geckos-04. All rights reserved.
+    </footer>
+  );
 };
 
 export default Footer;
--- src/@modules/home/Home.js
+++ src/@modules/home/Home.js
@@ -3,6 +3,7 @@ import ReactTooltip from 'react-tooltip';
 import styles from './Home.module.css';
 import SearchBar from '../search/SearchBar';
 import WorldMap from '../world-map/WorldMap';
+import Footer from '../common/components/footer/Footer';
 
 const Home = () => {
   const [content, setContent] = useState('');
@@ -12,6 +13,7 @@ const Home = () => {
       <SearchBar />
       <WorldMap setTooltipContent={setContent} />
       <ReactTooltip>{content}</ReactTooltip>
+      <Footer />
     </div>
   );
 };
