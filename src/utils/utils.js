6a8056552a03dcc3704684e696378dd3b63e36ec
Soyoung <cookr341@gmail.com>
2021-06-10 23:00:42 +0900

    refactor: Fix syntatic error

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,19 +1,21 @@
-export const calculateAirQuality = (pollutant) => {
-  let quality = "";
-  if (0 <= pollutant <= 50) {
-    quality = "Good";
-  } else if (51 <= pollutant <= 100) {
-    quality = "Moderate";
-  } else if (101 <= pollutant <= 150) {
-    quality = "Unhealthy for Sensitive Groups";
-  } else if (151 <= pollutant <= 200) {
-    quality = "Unhealthy";
-  } else if (201 <= pollutant <= 300) {
-    quality = "Very Unhealthy";
+export const calculateAirQuality = pollutant => {
+  console.log(pollutant);
+  let quality = '';
+  if (0 <= pollutant && pollutant <= 50) {
+    quality = 'Good';
+  } else if (51 <= pollutant && pollutant <= 100) {
+    quality = 'Moderate';
+  } else if (101 <= pollutant && pollutant <= 150) {
+    quality = 'Unhealthy for Sensitive Groups';
+  } else if (151 <= pollutant && pollutant <= 200) {
+    quality = 'Unhealthy';
+  } else if (201 <= pollutant && pollutant <= 300) {
+    quality = 'Very Unhealthy';
   } else if (301 <= pollutant) {
-    quality = "Hazardous";
+    quality = 'Hazardous';
   } else {
-    quality = "Unknown";
+    quality = 'Unknown';
   }
+  console.log(quality);
   return quality;
 };
