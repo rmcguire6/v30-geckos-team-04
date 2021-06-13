9c517fb4de99df69d2b39a5ef92fc5fb3037a48b
Regina Mc Guire <28935544+rmcguire6@users.noreply.github.com>
2021-06-13 09:05:13 -0700

    temp

--- src/@modules/region/Region.js
+++ src/@modules/region/Region.js
@@ -3,34 +3,50 @@ import axios from "axios";
 import styles from "./Region.module.css";
 import SearchBar from "../search/SearchBar";
 import CurrentContext from "../../context/Current";
-
+import Card from "../../@modules/common/components/card/Card";
 const Region = () => {
   const { currentLocation } = useContext(CurrentContext);
-  const [data, setData] = useState([]);
-  const [latAndLng, setLatAndLng] = useState();
+  const data = [48.856506, 2.352133];
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState("false")
+  // useEffect(() => {
+  //   const fetchLatAndLong = async () => {
+  //     try {
+  //       const result = await axios.get(
+  //         `http://www.mapquestapi.com/geocoding/v1/address?key=${process.env.REACT_APP_MAPQUEST}&location=${currentLocation}`
+  //       );
+  //       setData(result.data.results[0].locations[0].displayLatLng);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //     return data;
+  //   };
+  //   if (currentLocation.length > 0) {
+  //     fetchLatAndLong();
+  //   }
+  // }, [currentLocation]);
+  // console.log("loc data", currentLocation, data);
 
   useEffect(() => {
-    const fetchLatAndLong = async () => {
+    const fetchMeasurements = async (currentLocation) => {
       try {
+        setLoading("true")
         const result = await axios.get(
-          `http://www.mapquestapi.com/geocoding/v1/address?key=${process.env.REACT_APP_MAPQUEST}&location=${currentLocation}`
+          `https://docs.openaq.org/v2/latest?limit=100&page=1&offset=0&sort=desc&coordinates=48.856506%2C2.352133&radius=100&order_by=lastUpdated&dumpRaw=false`
         );
-        setData(result.data.results[0].locations[0].displayLatLng);
+        setResults(result.data.results);
+        setLoading("false")
+        console.log("results", results);
       } catch (error) {
+        setLoading("noResults")
         console.error(error);
       }
-      return data;
     };
-    if (currentLocation.length > 0) {
-      fetchLatAndLong();
+    if (currentLocation !== '') {
+      fetchMeasurements();
+      console.log("fetch happened");
     }
   }, [currentLocation]);
-  console.log("loc data", currentLocation, data);
-  useEffect(() => {
-    setLatAndLng([data.lat, data.lng]);
-    console.log("latLng changed", latAndLng);
-  }, [data.lat, data.lng]);
-
   return (
     <div className={styles.region}>
       <h1 className={styles.title}>Region</h1>
@@ -60,6 +76,16 @@ const Region = () => {
           <p className={styles.label}>No Location Selected</p>
         </div>
       )}
+      {loading === "true"? (
+        <p>Loading ...</p> 
+      ) : {loading === "noResults" ? <p className={styles.label}>No Measurements at this Location</p> : 
+     
+        <Card
+          location={results?.location}
+          country={results?.country}
+          measurements={results?.measurements}
+        />}
+      }
     </div>
   );
 };
