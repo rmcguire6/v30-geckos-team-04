c83059f7373b0b1e8917d4722446951222bf416f
Soyoung <cookr341@gmail.com>
2021-06-14 18:56:13 +0900

    task: Correct countries type

--- src/hooks/useGetCountriesDailyAverages.js
+++ src/hooks/useGetCountriesDailyAverages.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const useGetCountriesDailyAverages = () => {
-  const [countries, setCountries] = useState([]);
+  const [countries, setCountries] = useState({});
   useEffect(() => {
     const fetchCountriesAverages = async () => {
       const api = `https://docs.openaq.org/v2/averages?date_from=2021-06-11T00%3A00%3A00%2B00%3A00&date_to=2021-06-11T19%3A04%3A00%2B00%3A00&parameter=pm25&limit=200&page=1&offset=0&sort=desc&spatial=country&temporal=day&group=false`;
